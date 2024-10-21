1b86c027a08330b2b93594f5ec86fe2d0d1234e1
Tequam Niguse <tequam@Tequams-MacBook-Air.local>
2024-10-21 11:56:06 +0200

    coming soon

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] justify-between">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -12,8 +12,8 @@ export default function Home() {
           height={38}
           priority
         />
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <p>Coming soon!</p>
+        <div className="flex gap-8 items-center flex-col sm:flex-row self-center">
+          <h1 className="text-blue-700 text-4xl">Coming soon!</h1>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
